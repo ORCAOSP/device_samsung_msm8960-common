8c737a6088e20652e8d19be8659ef635d288cc13
orcaosp <orcaproject@yahoo.com>
2013-04-15 20:44:00 -0400

    msm8960-common updates

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,7 @@ BOARD_VENDOR := samsung
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_ARCH_VARIANT_CPU := cortex-a9
 
 # inherit from qcom-common
 -include device/samsung/qcom-common/BoardConfigCommon.mk
@@ -26,8 +27,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 # Architecture
 TARGET_CPU_SMP := true
 
-# Flags
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+# Flags for Krait CPU
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64 -DQCOM_BSP
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
@@ -36,6 +39,9 @@ TARGET_KRAIT_BIONIC_PLDOFFS := 10
 TARGET_KRAIT_BIONIC_PLDTHRESH := 10
 TARGET_KRAIT_BIONIC_BBTHRESH := 64
 TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# Adreno configuration
+BOARD_EGL_CFG := device/samsung/msm8960-common/configs/egl.cfg
 
 # Wifi related defines
 WIFI_BAND := 802_11_ABG
@@ -80,15 +86,11 @@ BOARD_USES_ALSA_AUDIO := true
 BOARD_HAVE_SAMSUNG_AUDIO := true
 BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
-
-# Workaround for missing symbols in camera
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-
-# We still have the old ION API
-BOARD_HAVE_OLD_ION_API := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 
 # Kernel time optimization
-KERNEL_HAS_GETTIMEOFDAY_HELPER := true
+# temp remove - causing issues with short/long presses
+# KERNEL_HAS_GETTIMEOFDAY_HELPER := true
 
 # We have the new GPS driver
 BOARD_HAVE_NEW_QC_GPS := true
